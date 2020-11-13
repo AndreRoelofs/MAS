673b18ea3d0ee59b0b527c027e6010d0f7f996af
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-11-13 15:16:24 +0100

    happiness

--- ass1/voting.py
+++ ass1/voting.py
@@ -4,33 +4,43 @@ import time
 import copy
 
 class Situation:
-    def __init__(self, voter_num, candidate_num, voting_scheme="Voting for one"):
+    def __init__(self,voter_num,candidate_num,voting_scheme="Voting for one"):
         self.voter_num = voter_num
         self.candidate_num = candidate_num
         self.voting_scheme = voting_scheme
         self.candidates = Candidates(self.candidate_num)
         self.voters = [Voter(self.candidates.num,
-                             voting_scheme=self.voting_scheme) for i in range(self.voter_num)]
-        self.situation = np.zeros((self.voter_num, self.candidate_num))
+        voting_scheme = self.voting_scheme) for i in range(self.voter_num)]
+        self.situation = np.zeros((self.voter_num,self.candidate_num))
         for i in range(self.voter_num):
             self.situation[i] = self.voters[i].preference
         self.situation = self.situation.T
         self.GetOutcome()
+        self.overallhappiness = 0.0
+        self.GetOverallHappiness()
 
     def GetOutcome(self):
         for voter in self.voters:
-            self.candidates.outcome = np.sum([self.candidates.outcome, voter.vote], axis=0)
+            self.candidates.outcome_value = np.sum([self.candidates.outcome_value,voter.vote],axis = 0)
+        self.candidates.outcome_order = np.argsort(self.candidates.outcome_value)
+
+    def GetOverallHappiness(self):
+        self.overallhappiness = 0.0
+        for voter in self.voters:
+            voter.GetHappiness(self.candidates.outcome_order)
+            self.overallhappiness += voter.happiness
 
     def Print(self):
-        print("the situation now is \n{}\n".format(self.situation))
+        print("the situation now is \n{}\noverallhappiness {}\n".format(self.situation,self.overallhappiness))
         self.candidates.Print()
         for voter in self.voters:
             voter.Print()
 
 class Voter:
-    def __init__(self, candidates_num, preference=None,
-                 vote_order=None, voting_scheme="Voting for one"):
+    def __init__(self,candidates_num,preference=None,
+    vote_order=None,voting_scheme = "Voting for one"):
         self.candidates_num = candidates_num
+        self.happiness = 0.0
         self.preference = preference
         self.vote_order = vote_order
         self.voting_scheme = voting_scheme
@@ -46,7 +56,7 @@ class Voter:
         random.shuffle(self.preference)
 
     def SetVotingStrategy(self):
-        self.vote_order = copy.copy(self.preference)  # voting order is the same as preference
+        self.vote_order = copy.copy(self.preference) #voting order is the same as preference
 
     def GetVote(self):
         if self.voting_scheme == "Voting for one":
@@ -61,23 +71,30 @@ class Voter:
             for i in range(self.candidates_num):
                 self.vote[self.vote_order[i]] = self.candidates_num-1-i
 
+    def GetHappiness(self,outcome_order):
+        self.happiness = 0.0
+        reversed_preference = list(reversed(self.preference))
+        for i in range(self.candidates_num):
+            self.happiness += (reversed_preference.index(i)+1) * (outcome_order.tolist().index(i)-reversed_preference.index(i))
+        self.happiness = 1/(1+abs(self.happiness))
+
     def Print(self):
         print("Voter: candidates_num {}\npreference {}\nvote_order {}\
-        \nvoting_scheme {}\nvote {}\n".format(
-            self.candidates_num, self.preference, self.vote_order,
-            self.voting_scheme, self.vote))
-
+        \nvoting_scheme {}\nvote {}\nhappiness {}\n".format(
+        self.candidates_num,self.preference,self.vote_order,
+        self.voting_scheme,self.vote,self.happiness))
 
 class Candidates:
-    def __init__(self, num):
+    def __init__(self,num):
         self.num = num
         self.ids = [i for i in range(self.num)]
-        self.outcome = [0 for i in range(self.num)]
+        self.outcome_value = [0 for i in range(self.num)]
+        self.outcome_order = [i for i in range(self.num)]
 
     def Print(self):
-        print("Candidates: num {}\nids {}\noutcome {}\n".format(
-            self.num, self.ids, self.outcome))
-
+        print("Candidates: num {}\nids {}\noutcome_value {}\
+        \noutcome_order {}\n".format(
+        self.num,self.ids,self.outcome_value,self.outcome_order))
 
 if __name__ == "__main__":
     voting_schemes = [
@@ -86,5 +103,5 @@ if __name__ == "__main__":
         'Veto voting',
         'Borda voting',
     ]
-    s = Situation(1,4,voting_scheme='Borda voting')
+    s = Situation(2,4,voting_scheme='Borda voting')
     s.Print()
