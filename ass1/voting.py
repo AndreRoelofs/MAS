cd8ed30d1d137e8c1995b451d48440c39aa0fdb9
abeldewit <abeldewit@gmail.com>
2020-11-13 14:49:09 +0100

    I get anxious from bad spacing/indentation

--- ass1/voting.py
+++ ass1/voting.py
@@ -4,14 +4,14 @@ import time
 import copy
 
 class Situation:
-    def __init__(self,voter_num,candidate_num,voting_scheme="Voting for one"):
+    def __init__(self, voter_num, candidate_num, voting_scheme="Voting for one"):
         self.voter_num = voter_num
         self.candidate_num = candidate_num
         self.voting_scheme = voting_scheme
         self.candidates = Candidates(self.candidate_num)
         self.voters = [Voter(self.candidates.num,
-        voting_scheme = self.voting_scheme) for i in range(self.voter_num)]
-        self.situation = np.zeros((self.voter_num,self.candidate_num))
+                             voting_scheme=self.voting_scheme) for i in range(self.voter_num)]
+        self.situation = np.zeros((self.voter_num, self.candidate_num))
         for i in range(self.voter_num):
             self.situation[i] = self.voters[i].preference
         self.situation = self.situation.T
@@ -19,7 +19,7 @@ class Situation:
 
     def GetOutcome(self):
         for voter in self.voters:
-            self.candidates.outcome = np.sum([self.candidates.outcome,voter.vote],axis = 0)
+            self.candidates.outcome = np.sum([self.candidates.outcome, voter.vote], axis=0)
 
     def Print(self):
         print("the situation now is \n{}\n".format(self.situation))
@@ -28,8 +28,8 @@ class Situation:
             voter.Print()
 
 class Voter:
-    def __init__(self,candidates_num,preference=None,
-    vote_order=None,voting_scheme = "Voting for one"):
+    def __init__(self, candidates_num, preference=None,
+                 vote_order=None, voting_scheme="Voting for one"):
         self.candidates_num = candidates_num
         self.preference = preference
         self.vote_order = vote_order
@@ -46,7 +46,7 @@ class Voter:
         random.shuffle(self.preference)
 
     def SetVotingStrategy(self):
-        self.vote_order = copy.copy(self.preference) #voting order is the same as preference
+        self.vote_order = copy.copy(self.preference)  # voting order is the same as preference
 
     def GetVote(self):
         if self.voting_scheme == "Voting for one":
@@ -64,18 +64,20 @@ class Voter:
     def Print(self):
         print("Voter: candidates_num {}\npreference {}\nvote_order {}\
         \nvoting_scheme {}\nvote {}\n".format(
-        self.candidates_num,self.preference,self.vote_order,
-        self.voting_scheme,self.vote))
+            self.candidates_num, self.preference, self.vote_order,
+            self.voting_scheme, self.vote))
+
 
 class Candidates:
-    def __init__(self,num):
+    def __init__(self, num):
         self.num = num
         self.ids = [i for i in range(self.num)]
         self.outcome = [0 for i in range(self.num)]
 
     def Print(self):
         print("Candidates: num {}\nids {}\noutcome {}\n".format(
-        self.num,self.ids,self.outcome))
+            self.num, self.ids, self.outcome))
+
 
 if __name__ == "__main__":
     voting_schemes = [
